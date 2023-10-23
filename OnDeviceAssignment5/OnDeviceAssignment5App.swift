23722680abe10fb5ec1030a3b3c4d22d5a66af8a
AlexanderAndersson <aaandersson98@gmail.com>
2023-10-23 13:05:54 +0200

    Initial Commit

---
+++ OnDeviceAssignment5/OnDeviceAssignment5App.swift
@@ -0,0 +1,17 @@
+//
+//  OnDeviceAssignment5App.swift
+//  OnDeviceAssignment5
+//
+//  Created by Alexander Andersson on 2023-10-23.
+//
+
+import SwiftUI
+
+@main
+struct OnDeviceAssignment5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
